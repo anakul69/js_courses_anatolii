4c5cfd0994713f2d737fcd53f5b3cb09d29a56db
Anatolii Kulinich <anatoly.k@payop.com>
2024-10-23 17:35:06 +0300

    [Lesson 1] homework

---
+++ main.js
@@ -0,0 +1,25 @@
+let userNickname = "Anatoliy";
+let userScore = 33;
+let age = 25;
+
+console.log(age);
+
+age = age + 5;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+isStudent = false;
+console.log(isStudent);
+
+let item = 10;
+console.log(item);
+
+item = "ten";
+console.log(item);
+
+item = true;
+console.log(item);
+
+
